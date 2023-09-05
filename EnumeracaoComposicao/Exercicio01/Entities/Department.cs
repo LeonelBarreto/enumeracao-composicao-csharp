926c2aaad9f6c28dae1f4f6d44394c69036e4a6e
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-09-05 15:46:32 -0300

    resolvido: exercicio 1

---
+++ EnumeracaoComposicao/Exercicio01/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercicio01.Entities
+{
+	public class Department
+	{
+		public string Name { get; set; }
+
+		public Department()
+		{
+
+		}
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
