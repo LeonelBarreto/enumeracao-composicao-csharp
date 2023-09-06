951ca8d81dc0097dedb5fefe88e31c2b67576588
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-09-06 14:14:35 -0300

    resolvido: exercicio 2

---
+++ EnumeracaoComposicao/Exercicio02/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Exercicio02.Entities
+{
+	public class Comment
+	{
+		public string Text { get; set; }
+
+		public Comment()
+		{
+		}
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
+
